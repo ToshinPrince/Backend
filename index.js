153c28c2d4b95741cc6db339603ecc60be16e96d
Toshin Prince <toshinprince@gmail.com>
2023-09-01 12:49:23 +0530

    inital_Commit

---
+++ index.js
@@ -0,0 +1,19 @@
+import express from "express";
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello</h1>");
+});
+
+app.get("/about", (req, res) => {
+  res.send("<h1>I am a developer</h1>");
+});
+
+app.get("/contact", (req, res) => {
+  res.send("<h1>Contact me through LinkedIn!</h1>");
+});
+
+app.listen(port, () => {
+  console.log(`server Running on port ${port}`);
+});
