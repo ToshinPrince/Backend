352359f81290841bcf785f789a73b20c97bca7a5
Toshin Prince <toshinprince@gmail.com>
2023-09-18 13:14:08 +0530

    Custom_Middleware

--- MiddleWares/index3.js
+++ MiddleWares/index3.js
@@ -3,6 +3,19 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+// There are Four Types of Middleware
+// Pre-Processing, Logging, Authentication, Error
+
+// Here we are making custom Middleware
+function logger(req, res, next) {
+  console.log("Request Method:", req.method);
+  console.log("Requiest URL:", req.url);
+  //next function is used to continue to the next thing
+  //the order is very imp
+  //if next not used then it will not move to next method
+  next();
+}
+
 app.use(logger);
 
 app.get("/", (req, res) => {
